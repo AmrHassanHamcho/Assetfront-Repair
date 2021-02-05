789aeeacb3ffa4a7b69b9c8b773ef5c594fa81db
AmrHamcho <amrhh@stud.ntnu.no>
2021-02-05 10:15:53 +0100

    spring boot and docker setup

---
+++ backend/src/main/java/bachelor/springboot/AssetrepairApplication.java
@@ -0,0 +1,13 @@
+package bachelor.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  AssetrepairApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetrepairApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/bachelor/springboot/AssetrepairApplicationTests.java
@@ -0,0 +1,13 @@
+package bachelor.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetrepairApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
