1d6636081d36ba31d6edb849649da7744fe00e01
Abdi Mohammad Bako <abdimb@stud.ntnu.no>
2021-03-30 18:35:07 +0200

    functionality to upload tcr to s3 bucket

--- frontend/src/app/tcr/tcr.component.ts
+++ frontend/src/app/tcr/tcr.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiRequestService} from '../API-request/api-request.service';
+import {PersonalDataComponent} from './personal-data/personal-data.component';
+import {Router} from '@angular/router';
+import {TcrService} from './tcr.service';
 
 @Component({
   selector: 'app-tcr',
@@ -7,36 +10,63 @@ import {ApiRequestService} from '../API-request/api-request.service';
   styleUrls: ['./tcr.component.scss']
 })
 export class TcrComponent implements OnInit {
+  private allSelected = false;
+  constructor(public request: ApiRequestService,
+              private router: Router,
+              public tcr: TcrService) {
+  }
+
   searchedSerialNo: boolean;
   color = 'lightblue';
   ttrCopy = this.request.getAssetDetails()[0];
-   constructor(public request: ApiRequestService) {
-  }
+  // logToconsole(indexTcr: number, indexCp: number){
+  // console.log(this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value);
+  // }
+  tests = [];
 
   ngOnInit(): void {
   }
+
   onSuccessfullSearch(): boolean {
-    if (this.ttrCopy === undefined){
+    if (this.ttrCopy === undefined) {
       this.searchedSerialNo = false;
       return this.searchedSerialNo;
 
-    }else
-    {
+    } else {
       this.searchedSerialNo = true;
       return this.searchedSerialNo;
 
     }
   }
-  updateValue(value: number, indexTcr: number, indexCp: number){
-      this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value = value;
-      return  this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value;
-   }
-  // logToconsole(indexTcr: number, indexCp: number){
-    // console.log(this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value);
-  // }
-printHeleArray(){
- console.log(this.ttrCopy);
+
+  updateValue(value: number, indexTcr: number, indexCp: number) {
+    this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value = value;
+    return this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value;
+  }
+
+  printHeleArray() {
+    // console.log(this.ttrCopy);
+    this.tcr.setTcr(this.ttrCopy);
+  }
+
+  personData() {
+    // if (this.allFilled())  {
+      this.router.navigate(['/tcr/personal-data']);
+    // }
+  }
+
+  allFilled() {
+    for (const tcr of this.ttrCopy.tcr) {
+      for (const cp of tcr.checkpoint) {
+        if (cp.value > 0) {
+          this.tests.push(true);
+        } else {
+          this.tests.push(false);
+        }
+      }
+    }
+    this.allSelected = this.tests.every(v => v === false);
+    return this.allSelected;
   }
 }
-
 
