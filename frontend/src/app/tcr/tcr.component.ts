d34d9aad2f772afdf7a544b34075d6d1717a4f6a
Abdi Mohammad Bako <abdimb@stud.ntnu.no>
2021-04-04 23:03:38 +0200

    added functionality to tick when tcr is filled

--- frontend/src/app/tcr/tcr.component.ts
+++ frontend/src/app/tcr/tcr.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, EventEmitter, OnInit, Output, ViewChildren} from '@angular/core';
 import {ApiRequestService} from '../API-request/api-request.service';
-import {PersonalDataComponent} from './personal-data/personal-data.component';
 import {Router} from '@angular/router';
 import {TcrService} from './tcr.service';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
@@ -10,30 +9,55 @@ import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
   templateUrl: './tcr.component.html',
   styleUrls: ['./tcr.component.scss']
 })
-export class TcrComponent implements OnInit {
+export class TcrComponent implements OnInit,  AfterViewChecked {
   constructor(public request: ApiRequestService,
               private router: Router,
               public tcr: TcrService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private changeRef: ChangeDetectorRef) {
   }
+
+  abdi = false;
+  all = [];
+   teller = 0;
 
   allSelected = false;
 
   searchedSerialNo: boolean;
   color = 'lightblue';
   ttrCopy = this.request.getAssetDetails()[0];
-  // logToconsole(indexTcr: number, indexCp: number){
-  // console.log(this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value);
-  // }
+
   tests = [];
   registerForm = this.formBuilder.group({
     selected: ['', [Validators.required]]
 
   });
 
-  ngOnInit(): void {
-  }
+  /*allFilled() {
+     for (const t of this.ttrCopy.tcr) {
+       for (const cp of t.checkpoint) {
+         t.checkpoint.forEach((c, index) => {
+           this.all[index] = false;
+           if (cp[index].value > -1) {
+             this.all[index] = true;
+           }
+         });
+       }
+       this.allSelected = this.tests.every(v => v === true);
+     }
+   }
+*/
+  notFilled = '';
+ /* hide = true;
+  indexCp = 0;
+  counter = 0;
 
+  addCounter() {
+    this.counter += 1;
+  }*/
+  ngOnInit(): void {
+
+  }
   onSuccessfullSearch(): boolean {
     if (this.ttrCopy === undefined) {
       this.searchedSerialNo = false;
@@ -45,10 +69,37 @@ export class TcrComponent implements OnInit {
     }
   }
 
-  updateValue(value: number, indexTcr: number, indexCp: number) {
-    this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value = value;
-    this.allFilled();
-    return this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value;
+  updateValue(value: number, indexTcr: number, indexCp: number ) {
+  this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value = value;
+    // this.allFilled();
+   // this.selectedTrigger();
+    /*this.tcrIndex = indexTcr;
+    this.checkPointIndex = indexCp;
+    this.indexCp = indexCp;
+    this.hide = false;*/
+    // this.selected(indexTcr, this.ttrCopy.tcr[indexTcr].checkpoint.length);
+    /* if (this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value > -1 &&
+          indexCp !== this.checkPointIndex){
+              this.counter ++;
+            }
+
+
+        console.log('length of checkpoint : ' + this.ttrCopy.tcr[indexTcr].checkpoint.length);
+        console.log('counter' + this.counter);
+
+        if (this.counter === this.ttrCopy.tcr[indexTcr].checkpoint.length){
+              console.log('amr kommmet hit');
+              this.x = true;
+              this.counter = 0;
+        }
+        if (indexTcr !== this.tcrIndex){
+          console.log('we have come into tcr index');
+          this.counter = 0;
+        }
+        this.checkPointIndex = indexCp;
+        this.tcrIndex = indexTcr;
+       */
+  return this.ttrCopy.tcr[indexTcr].checkpoint[indexCp].value;
   }
 
   printHeleArray() {
@@ -61,9 +112,9 @@ export class TcrComponent implements OnInit {
     this.router.navigate(['/tcr/personal-data']);
     // }
   }
-
+/*
   allFilled() {
-    /* let counter = 0;
+    /!* let counter = 0;
      let checker = [];
      let b = false;
      for (const t of this.ttrCopy.tcr) {
@@ -79,8 +130,9 @@ export class TcrComponent implements OnInit {
      console.log('*****' + this.tests);
 
    }
- */      //  this.allSelected = this.tests.every(v => v === true);
+ *!/      //  this.allSelected = this.tests.every(v => v === true);
     // console.log(this.tests);
+    // tslint:disable-next-line:prefer-for-of
     for(let tcrI = 0; tcrI < this.ttrCopy.tcr.length; tcrI++){
       for (let cpi = 0; cpi < this.ttrCopy.tcr[tcrI].checkpoint.length; cpi++){
 
@@ -94,5 +146,81 @@ export class TcrComponent implements OnInit {
       }
     }
     console.log(this.tests);
+  }*/
+/*
+  selectedTrigger(){
+    for (let itcr = 0; itcr < this.ttrCopy.tcr.length; itcr++){
+      for (let cp = 0; cp < this.ttrCopy.tcr[itcr].checkpoint; cp++){
+        if (this.ttrCopy.tcr[itcr].checkpoint[cp].value > -1){
+          this.x = true;
+        }
+      }
+    }
+  }
+*/
+
+
+  ngAfterViewChecked(): void { this.changeRef.detectChanges(); }
+ /* selected(indextcr: number, indexCp: number) {
+    for (let i = 0; i < indextcr; i++){
+      for (let j = 0; j < indexCp; j++){
+        this.tests[j] = false;
+        if (this.ttrCopy.tcr[indextcr].checkpoint[j].value > -1){
+          this.tests[j] = true;
+        }
+      }
+
+    }
+    this.abdi = this.tests.every(v => v === true);
+  }
+  tickCounter() {
+    this.teller++;
+  }
+  logtoConsole(){
+    console.log('ajab ' + this.teller++);
+  }
+ */ /*selectedCp(tcri: number, cpi: number){
+    for (let i = 0; i < cpi; i++){
+      this.tests[i] = false;
+      if (this.ttrCopy.tcr[tcri].checkpoint.value > -1){
+        this.tests[i] = true;
+      }
+    }
+ //   this.allSelected = this.tests.every(v => v === true);
+    console.log('Selected: ' + this.allSelected);
+  }*/
+/* count(i: number) {
+      return new Array(i);
+  }*/
+  good(tcri: number) {
+    this.abdi = false;
+    this.tests = [];
+    for ( let i = 0; i < this.ttrCopy.tcr[tcri].checkpoint.length; i ++){
+      this.tests[i] = false;
+          if (this.ttrCopy.tcr[tcri].checkpoint[i].value > -1){
+            this.tests[i] = true;
+          }
+   }
+    this.abdi = this.tests.every(v => v === true);
+  }
+  allFilled(){
+    for (let tcri = 0; tcri < this.ttrCopy.tcr.length; tcri++){
+      for ( let cpi = 0; cpi < this.ttrCopy.tcr[tcri].checkpoint.length; cpi ++){
+        this.all[cpi] = false;
+        if (this.ttrCopy.tcr[tcri].checkpoint[cpi].value > -1){
+          this.all[cpi] = true;
+
+        }
+
+      }
+
+    }
+    this.allSelected = this.all.every(v => v === true);
+
+  }
+
+  setNotFilled(notFilled: string) {
+    this.notFilled = notFilled;
+
   }
 }
