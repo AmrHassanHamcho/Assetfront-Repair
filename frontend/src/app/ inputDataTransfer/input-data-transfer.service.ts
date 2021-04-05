7acb10c245c720fc46c9ea92486514ed6f98252d
Mustafa Abdullah <mustafaa@stud.ntnu.no>
2021-04-05 10:58:00 +0200

    Inspection and Service

--- frontend/src/app/ inputDataTransfer/input-data-transfer.service.ts
+++ frontend/src/app/ inputDataTransfer/input-data-transfer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Validators} from "@angular/forms";
+import {PDFService} from '../PDF/pdf.service';
+import {NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels} from '@techiediaries/ngx-qrcode';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,19 @@ export class InputDataTransferService {
   coast;
   inspectionState;
 
-  constructor() { }
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = 'https://assetfront.com';
 
+  constructor( private PDF: PDFService ) {}
 
+   callPdfInspection()
+  {
+    this.PDF.Inspection(this.company, this.fName + ' ' + this.lName, this.date, this.inspectionState, this.Email, this.phone);
+  }
+
+  callPdfService()
+  {
+    this.PDF.Service(this.company, this.fName + ' ' + this.lName, this.date, this.hours, this.coast, this.comment, this.Email, this.phone);
+  }
 }
