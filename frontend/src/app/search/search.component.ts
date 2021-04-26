5362de3d04e35da6860149516f5f832b9e66940f
Abdi Mohammad Bako <abdimb@stud.ntnu.no>
2021-04-27 00:28:32 +0200

    maped to promise so that we get all data before processing, fixed bug in confirmation dialog

--- frontend/src/app/search/search.component.ts
+++ frontend/src/app/search/search.component.ts
@@ -1,10 +1,10 @@
-
 import {Component, OnInit, ViewEncapsulation, AfterViewInit, ViewChild} from '@angular/core';
 
 import {ApiRequestService} from '../API-request/api-request.service';
 import {MatDialog, MatDialogConfig, MatDialogRef} from '@angular/material/dialog';
 import {DialogWindowComponent} from './dialog-window/dialog-window.component';
 import {VehiclesService} from '../../vehicle-service/vehicle.service';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -13,6 +13,7 @@ import {VehiclesService} from '../../vehicle-service/vehicle.service';
   styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
+  errorMessage = '';
 
   constructor(
     public request: ApiRequestService,
@@ -23,28 +24,23 @@ export class SearchComponent implements OnInit {
 
   qrResultString: string;
   ShowHide = false;
+  loading = false;
   ngOnInit(): void {
   }
 
   openDialog() {
     const dialogConfig = new MatDialogConfig();
-    if (this.vehicle.getSerNo() !== undefined){
-      dialogConfig.autoFocus = true;
-      this.dialog.open(DialogWindowComponent, dialogConfig);
-    }
-      else{
-        this.dialog.closeAll();
-        dialogConfig.autoFocus = false;
-    }
+    //  dialogConfig.autoFocus = true;
+    this.dialog.open(DialogWindowComponent, dialogConfig);
+
   }
 
   showDiv(){
     if (!this.ShowHide){
-    this.ShowHide = true;
+      this.ShowHide = true;
     }
     else{
       this.ShowHide = false;
-      // document.getElementById('QR-Window').style.display = 'none';
       console.log(this.ShowHide);
     }
   }
@@ -55,21 +51,46 @@ export class SearchComponent implements OnInit {
 
   }
 
-  /**
-   * @param value of serial number entered by a user
-   */
-
-  // setSerialNo(value: string): void {
-  //   if (value) { // calls request.getVehicleData() if and only if the value is entered
-  //     // if not does nothing
-  //     this.request.getVehicleData(value)
-  //       // assigns deta recieved from observable to this local SearchComponent property
-  //       .subscribe(data => this.vehcilesDetail = data);
-  //
-  //
-  //   }
-
-  // }
-
-}
-
+/**
+ * @param value of serial number entered by a user
+ // tslint:disable-next-line:jsdoc-format
+ // setSerialNo(value: string): void {
+            // tslint:disable-next-line:jsdoc-format
+            if (value) { // calls request.getVehicleData() if and only if the value is entered
+         // tslint:disable-next-line:jsdoc-format
+         //     // if not does nothing
+         //     this.request.getVehicleData(value)
+ assigns deta recieved from observable to this local SearchComponent property
+.subscribe(data => this.vehcilesDetail = data); }
+ }
+**/
+ test(value: string) {
+         const regExpr = new RegExp(/^[A-Za-z0-9]/);
+         if (value.match(regExpr)) {
+         this.loading = true;
+         try {
+         this.request.setSerialNo(value).then(r => {
+         this.loading = false;
+         if (this.request.statusCode !== 200) {
+         console.log( 'this.request.statusCode');
+         }else {  if (this.request.assetDetails.length > 0) {
+         this.openDialog();
+         } else {
+         this.errorMessage = 'Invalid VIN(Vehicle Identification Number)';
+         }}
+         });
+         } catch (error){
+         console.log(error + 'From Test');
+         }
+         }
+         }
+ inputValidation($event: KeyboardEvent): boolean{
+         const regExpr = new RegExp(/^[A-Za-z0-9]/);
+         console.log($event.key);
+         if ($event.key.match(regExpr)) {
+         return true;
+         } else {
+         return false;
+         }
+         }
+ }
